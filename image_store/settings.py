b4c4834086cfd926a8ffe6df3ea65a7dbe5ec9ef
Aditya Nishad <adityanishad292@gmail.com>
2024-05-18 11:48:32 +0530

    added*

--- image_store/settings.py
+++ image_store/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1s6vqyf@%&yl=%5$)w5aliv_fs+$!z_z!waet3a56!b(ayu-$e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
